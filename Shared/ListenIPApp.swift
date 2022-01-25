2dfb9654fc0ea80bec6430f34a868f6e43c1f8f0
king <3925@winnermedical.com>
2022-01-25 09:20:22 +0800

    Initial Commit

---
+++ Shared/ListenIPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListenIPApp.swift
+//  Shared
+//
+//  Created by 张行 on 2022/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ListenIPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
